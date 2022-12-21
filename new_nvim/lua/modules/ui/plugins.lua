e0d057c16149caf4fe8a8d6716fe7e60a3068e78
double12gzh <double12gzh@163.com>
2022-12-21 18:49:58 +0800

    code format

--- new_nvim/lua/modules/ui/plugins.lua
+++ new_nvim/lua/modules/ui/plugins.lua
@@ -1,60 +1,61 @@
 local ui = {}
 
 ui["goolord/alpha-nvim"] = {
-    opt = true,
-    event = "BufWinEnter",
-    config = function()
-        require("modules.ui.alpha")
-    end,
+	opt = true,
+	event = "BufWinEnter",
+	config = function()
+		require("modules.ui.alpha")
+	end,
 }
 ui["akinsho/bufferline.nvim"] = {
-    opt = true,
-    tag = "*",
-    event = "BufReadPost",
-    config = function()
-        require("modules.ui.bufferline")
-    end,
+	opt = true,
+	tag = "*",
+	event = "BufReadPost",
+	config = function()
+		require("modules.ui.bufferline")
+	end,
 }
 ui["stevearc/dressing.nvim"] = {
-    opt = true,
-    event = "BufReadPost",
-    config = function()
-        require("modules.ui.dressing")
-    end,
+	opt = true,
+	event = "BufReadPost",
+	config = function()
+		require("modules.ui.dressing")
+	end,
 }
 ui["NvChad/nvim-colorizer.lua"] = {
-    opt = true,
-    after = { "nvim-treesitter" },
-    config = function()
-        require("modules.ui.colorizer")
-    end,
+	opt = true,
+	after = { "nvim-treesitter" },
+	config = function()
+		require("modules.ui.colorizer")
+	end,
 }
 ui["j-hui/fidget.nvim"] = {
-    opt = true,
-    event = "BufReadPost",
-    config = function()
-        require("modules.ui.fidget")
-    end,
+	opt = true,
+	event = "BufReadPost",
+	config = function()
+		require("modules.ui.fidget")
+	end,
 }
 ui["f-person/git-blame.nvim"] = {
-    opt = true,
-    event = "BufReadPost",
-    config = function()
-        require("modules.ui.git-blame")
-    end,
+	opt = true,
+	event = "BufReadPost",
+	config = function()
+		require("modules.ui.git-blame")
+	end,
 }
 ui["lewis6991/gitsigns.nvim"] = {
-    opt = true,
-    event = { "BufReadPost", "BufNewFile" },
-    config = function()
-        require("modules.ui.gitsigns")
-    end,
+	opt = true,
+	event = { "BufReadPost", "BufNewFile" },
+	config = function()
+		require("modules.ui.gitsigns")
+	end,
 }
 ui["sainnhe/gruvbox-material"] = {
-    opt = false,
-    config = function()
-        require("modules.ui.gruvbox-material")
-    end,
+	opt = false,
+	--event = "VimEnter",
+	config = function()
+		require("modules.ui.gruvbox-material")
+	end,
 }
 -- ui["ellisonleao/gruvbox.nvim"] = {
 -- 	opt = true,
@@ -66,56 +67,56 @@ ui["sainnhe/gruvbox-material"] = {
 -- 	opt = true,
 -- }
 ui["kevinhwang91/nvim-hlslens"] = {
-    opt = true,
-    module = "hlslens",
-    keys = { "/", "?", "*", "#" }, --'n', 'N', '*', '#', 'g'
-    config = function()
-        require("modules.ui.hlslens")
-    end,
+	opt = true,
+	module = "hlslens",
+	keys = { "/", "?", "*", "#" }, --'n', 'N', '*', '#', 'g'
+	config = function()
+		require("modules.ui.hlslens")
+	end,
 }
 ui["lukas-reineke/indent-blankline.nvim"] = {
-    opt = true,
-    event = "BufReadPost",
-    after = "nvim-treesitter",
-    config = function()
-        require("modules.ui.indent-blankline")
-    end,
+	opt = true,
+	event = "BufReadPost",
+	after = "nvim-treesitter",
+	config = function()
+		require("modules.ui.indent-blankline")
+	end,
 }
 ui["rebelot/kanagawa.nvim"] = {
-    opt = false,
-    config = function()
-        require("modules.ui.kanagawa")
-    end,
+	opt = false,
+	config = function()
+		require("modules.ui.kanagawa")
+	end,
 }
 ui["hoob3rt/lualine.nvim"] = {
-    opt = true,
-    after = { "git-blame.nvim" },
-    config = function()
-        require("modules.ui.lualine")
-    end,
+	opt = true,
+	after = { "git-blame.nvim" },
+	config = function()
+		require("modules.ui.lualine")
+	end,
 }
 ui["zbirenbaum/neodim"] = {
-    opt = true,
-    event = "LspAttach",
-    config = function()
-        require("modules.ui.neodim")
-    end,
+	opt = true,
+	event = "LspAttach",
+	config = function()
+		require("modules.ui.neodim")
+	end,
 }
 ui["romainl/vim-cool"] = {
-    opt = true,
-    after = { "nvim-hlslens" },
+	opt = true,
+	after = { "nvim-hlslens" },
 }
 ui["rcarriga/nvim-notify"] = {
-    opt = true,
-    event = "BufReadPre",
-    module = "notify",
-    config = function()
-        require("modules.ui.notify")
-    end,
+	opt = true,
+	event = "BufReadPre",
+	module = "notify",
+	config = function()
+		require("modules.ui.notify")
+	end,
 }
 ui["kyazdani42/nvim-web-devicons"] = {
-    opt = true,
-    module = "nvim-web-devicons",
+	opt = true,
+	module = "nvim-web-devicons",
 }
 -- ui["anuvyklack/pretty-fold.nvim"] = { -- not using fold ATM
 -- 	opt = true,
@@ -125,12 +126,12 @@ ui["kyazdani42/nvim-web-devicons"] = {
 -- 	end,
 -- }
 ui["petertriho/nvim-scrollbar"] = {
-    opt = true,
-    event = "BufReadPost",
-    module = "scrollbar.handlers.search",
-    config = function()
-        require("modules.ui.scrollbar")
-    end,
+	opt = true,
+	event = "BufReadPost",
+	module = "scrollbar.handlers.search",
+	config = function()
+		require("modules.ui.scrollbar")
+	end,
 }
 -- ui["folke/styler.nvim"] = {
 -- 	opt = true,
@@ -141,25 +142,25 @@ ui["petertriho/nvim-scrollbar"] = {
 -- 	end,
 -- }
 ui["levouh/tint.nvim"] = {
-    opt = true,
-    event = { "BufReadPost" },
-    config = function()
-        require("modules.ui.tint")
-    end,
+	opt = true,
+	event = { "BufReadPost" },
+	config = function()
+		require("modules.ui.tint")
+	end,
 }
 ui["folke/todo-comments.nvim"] = {
-    opt = true,
-    event = "BufReadPost",
-    config = function()
-        require("modules.ui.todo-comments")
-    end,
+	opt = true,
+	event = "BufReadPost",
+	config = function()
+		require("modules.ui.todo-comments")
+	end,
 }
 ui["itchyny/vim-highlighturl"] = {
-    opt = true,
-    event = "BufReadPost",
-    config = function()
-        vim.g.highlighturl_guifg = "#8AB4F8"
-    end,
+	opt = true,
+	event = "BufReadPost",
+	config = function()
+		vim.g.highlighturl_guifg = "#8AB4F8"
+	end,
 }
 
 return ui
